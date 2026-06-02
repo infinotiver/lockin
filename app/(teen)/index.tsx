f171c1d5f879a91e68ee5dabb16883e74f52650e
infinotiver <88645086+infinotiver@users.noreply.github.com>
2026-06-02 22:17:08 +0530

    feat!: restructure app layout and add parent/teen screens
    BREAKING CHANGE: delete (tabs) folder, app tabs are now handled separately by a _layout.tsx and sub dirs

---
+++ app/(teen)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function TeenHome() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Teen Home</Text>
+    </View>
+  );
+}
