1bd0b70ec0bd02a96cf0c135b9168629aabf526d
infinotiver <88645086+infinotiver@users.noreply.github.com>
2026-06-23 19:23:04 +0530

    chore: add todo

--- types/stakes.ts
+++ types/stakes.ts
@@ -2,10 +2,11 @@ export type StakeStatus =
   | "available" // up coming
   | "active" // In progress
   | "completed" // Submitted proof
-  | "approved" // teen's request approved (teen only)
+  | "approved" // teen's request approved (everything settled for teen)
   | "rejected" // Parent called BS (teen only)
   | "expired"; // dont have a clock???
 
+// TODO: stake statuses can be misleading and needs reforms
 export type QuestType = "chore" | "study" | "screen-time" | "work" | "shop";
 
 export type Stake = {
